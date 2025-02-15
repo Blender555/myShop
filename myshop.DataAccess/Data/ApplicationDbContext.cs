0cc35434c0e36fc1419814892f82a0a1b37dbf53
Hossam Refaat <hossamrefaat.dev@gmail.com>
2025-02-15 18:48:42 +0200

    Initial commit

---
+++ myshop.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myshop.Entities.Models;
+
+namespace myshop.DataAccess
+{
+    public class ApplicationDbContext: DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
